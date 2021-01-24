814ac060c325cf66f922d51910226e796b3649a1
TAKU <taku.tech521@gmail.com>
2021-01-24 22:18:52 +0900

    課題実施前まで完成

---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 木村貴弘 on 2021/01/23.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // カテゴリ
+    @objc dynamic var category = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    // 日時
+    @objc dynamic var date = Date()
+    
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
